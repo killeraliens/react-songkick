ef611e693790b6ef1da92d22bf4e00bf97b82f02
alexandra campbell <killeraliens@outlook.com>
2019-05-14 11:44:40 -0700

    search value state

--- src/components/search_by_name.jsx
+++ src/components/search_by_name.jsx
@@ -1,14 +1,27 @@
 import React, { Component} from 'react';
 
 class SearchByName extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ""
+    }
+  }
+
+  updateValue = (event) => {
+    this.setState({
+      term: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="search-input-contain">
-        <input className="form-control search-input"></input>
+        <input value={this.state.term} className="form-control search-input" onChange={this.updateValue}></input>
       </div>
     )
   }
-
 }
 
 export default SearchByName;
